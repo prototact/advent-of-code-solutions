4e15453193ab08194f5be9d9098d414ffa3f56cf
Orfeas Karachalios <prototact@pm.me>
2023-12-13 10:55:36 +0200

    fixed alternative way to count orderings but it is even slower (exponential size due to doubling on every wildcard)

--- 2023/day12/Main.hs
+++ 2023/day12/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Data.List (break, intercalate, splitAt, uncons)
+import Data.List (break, intercalate, splitAt, uncons, unlines, nub)
 import Data.Char (isSpace, isDigit, isPunctuation)
 import Control.Exception (assert)
 
@@ -14,7 +14,7 @@ breakOn pred xs = actual
         (start, rest) = break pred xs
         actual = case uncons rest of
             Nothing -> (start, Nothing)
-            Just (x, xs) -> (start, Just xs)                        
+            Just (x, xs) -> (start, Just xs)
 
 splitNumbers :: String -> [Int]
 splitNumbers = reverse . go []
@@ -62,20 +62,59 @@ countOrderings (group:groups) (size:sizes)
             alignSizes :: Int -> [Spring] -> Int
             alignSizes size xs = sum [countMore (split idx) | idx <- [0..length xs - size]]
                 where
-                    split idx = let (start, rest) = splitAt idx xs in let (_, end) = splitAt size rest in (start, end) 
+                    split idx = let (start, rest) = splitAt idx xs in let (_, end) = splitAt size rest in (start, end)
                     countMore (start, []) = if all (== Wildcard) start then countOrderings groups sizes else 0
                     countMore (start, x:xs) = if all (== Wildcard) start && x == Wildcard then countOrderings (xs:groups) sizes else 0
-                 
+
+
+count :: [Spring] -> [Int]
+count = filter (/= 0) . go
+    where
+        go [] = [0]
+        go (Broken:xs) = let (c:cs) = go xs in (c + 1:cs)
+        go (Okay:xs) = 0:go xs
+        go (Wildcard:xs) = go xs
+
+match' :: [Int] -> [Int] -> Bool
+match' _ [] = True
+match' sizes xs = let ((x,y):zs) = reverse (zip xs sizes) in all (uncurry (==)) zs && x <= y
+
+fill :: [Int] -> [Spring] -> [[Spring]]
+fill sizes = go
+    where
+        go [] = [[]]
+        go (Wildcard:xs) = [x:zs | x <- [Okay, Broken],
+                                zs <- go xs,
+                                let start = count . reverse $ (x:zs),
+                                match' sizes start]
+        go (Broken:xs) = filter (\x -> let start = count . reverse $ x in match' sizes start) . map (Broken:) . go $ xs
+        go (Okay:xs) = error "should never occur"
+
+countOrderings' :: [[Spring]] -> [Int] -> Int
+countOrderings' [] sizes = if not . null $ sizes then 0 else 1
+countOrderings' groups [] = if all (all (== Wildcard)) groups then 1 else 0
+countOrderings' (group:groups) sizes = sum [countOrderings' groups (drop (length matched) sizes) | matched <- getMatches group]
+    where
+        match :: [Int] -> Bool
+        match x = (length x <= length sizes) && (and . zipWith (==) sizes $ x)
+        getMatches :: [Spring] -> [[Int]]
+        getMatches = filter match . map (count . reverse) . fill sizes . reverse
+
 main :: IO ()
 main = do
     file <- readFile "sample.txt"
     let springrows = parseSpringRows file
+    let filled = filter (and . zipWith (==) [3, 2, 1]) . map (count . reverse) . fill [3,2,1] . reverse . parseSprings $ "?###????????"
+    let grouped =  map (\(group, sizes) -> (breakGroups group, sizes)) springrows
+    let expected = map (uncurry countOrderings') grouped
+    print expected
+    print $ assert (sum expected == 21) "small test ok!"
     let multiplied = map (uncurry multiplyBy5) springrows
     let grouped = map (\(group, sizes) -> (breakGroups group, sizes)) multiplied
-    print $ assert (sum (map (uncurry countOrderings) grouped) == 525152) "count ok!"
+    print $ assert (sum (map (uncurry countOrderings') grouped) == 525152) "count ok!"
     -- test over
     file <- readFile "input.txt"
     let springrows = parseSpringRows file
     let multiplied = map (uncurry multiplyBy5) springrows
     let grouped = map (\(group, sizes) -> (breakGroups group, sizes)) multiplied
-    print $ sum (map (uncurry countOrderings) grouped)
+    print $ sum (map (uncurry countOrderings') grouped)
